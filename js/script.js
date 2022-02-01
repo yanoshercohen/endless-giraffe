eb40003ff5b50cca05b316850e15f0a9a154a8a5
Yan Osher Cohen <74152828+yanoshercohen@users.noreply.github.com>
2022-02-01 22:28:35 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-
 //document.querySelectorAll("img").length-2 for counting the templates
 function showContent() {
     var e = document.getElementsByTagName("template")[0].content.cloneNode(true);
     document.body.appendChild(e)
 }
+
 showContent(), window.addEventListener("scroll", function() {
-    document.documentElement.getBoundingClientRect().bottom < 3* document.documentElement.clientHeight && showContent()
+    document.documentElement.getBoundingClientRect().bottom < 3 * document.documentElement.clientHeight && showContent()
 });
